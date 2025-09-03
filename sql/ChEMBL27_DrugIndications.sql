07ac8ed599c97e14f2f4b75fce8088535c9c06e4
JiamingHu <96317040+jxh10111@users.noreply.github.com>
2025-09-03 18:37:10 -0400

    Delete sql directory

--- sql/ChEMBL27_DrugIndications.sql
+++
@@ -1,4 +0,0 @@
-SELECT md.chembl_id, md.pref_name, di.max_phase_for_ind, di.mesh_id, di.mesh_heading, di.efo_id, di.efo_term
-	FROM public.drug_indication di
-	INNER JOIN public.molecule_dictionary md
-		ON di.molregno = md.molregno
